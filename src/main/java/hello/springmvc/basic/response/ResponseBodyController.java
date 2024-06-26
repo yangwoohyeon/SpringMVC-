b612785c87603e2ed6fff3d99201eca5818f3ce3
yangwoohyeon <dngus0717@gmail.com>
2024-06-26 20:48:31 +0900

    Spring 강의

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -10,22 +10,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Slf4j
-@Controller
+//@Controller
+//@ResponseBody
+@RestController //@Controller + @ResponseBody
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
-    public void responseBodyV1(HttpServletResponse response){
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
         response.getWriter().write("ok");
     }
     @GetMapping("/response-body-string-v2")
     public ResponseEntity<String> responseBodyV2(HttpServletResponse response){
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
-    @ResponseBody
+    //@ResponseBody
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3(HttpServletResponse response){
         return "ok";
@@ -39,7 +42,7 @@ public class ResponseBodyController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
+    //@ResponseBody
     @GetMapping("/response-body-Json-v2")
     public HelloData responseBodyJsonV2(){
         HelloData helloData = new HelloData();
